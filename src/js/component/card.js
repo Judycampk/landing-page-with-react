f3928c38d108ff9e41c9d4b78ebe8a45737ad923
Judycampk <judhycampk71@outlook.com>
2021-02-05 22:28:38 +0000

    Landing page with react

---
+++ src/js/component/card.js
@@ -0,0 +1,76 @@
+import React from "react";
+
+export const Card = () => {
+	return (
+		<div className="card-deck">
+			<div className="card text-center">
+				<img
+					src="http://images.unsplash.com/photo-1444703686981-a3abbc4d4fe3?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxMjA3fDB8MXxzZWFyY2h8MXx8dW5pdmVyc2V8fDB8fHw&ixlib=rb-1.2.1&q=80&w=1080"
+					className="card-img-top"
+					alt="..."
+				/>
+				<div className="card-body">
+					<h5 className="card-title">Card title</h5>
+					<p className="card-text">
+						Some quick example text to build on the card title and
+						make up the bulk of the cards content.
+					</p>
+					<a href="#" className="btn btn-primary">
+						Go somewhere
+					</a>
+				</div>
+			</div>
+			<div className="card text-center">
+				<img
+					src="https://pbs.twimg.com/media/DeobfLYUcAAsZ77.jpg"
+					className="card-img-top"
+					alt="..."
+				/>
+				<div className="card-body">
+					<h5 className="card-title">Card title</h5>
+					<p className="card-text">
+						Some quick example text to build on the card title and
+						make up the bulk of the cards content.
+					</p>
+					<a href="#" className="btn btn-primary">
+						Go somewhere
+					</a>
+				</div>
+			</div>
+			<div className="card text-center">
+				<img
+					src="https://pbs.twimg.com/media/DX05NKIUQAAdWva.jpg"
+					className="card-img-top"
+					alt="..."
+				/>
+				<div className="card-body">
+					<h5 className="card-title">Card title</h5>
+					<p className="card-text">
+						Some quick example text to build on the card title and
+						make up the bulk of the cards content.
+					</p>
+					<a href="#" className="btn btn-primary">
+						Go somewhere
+					</a>
+				</div>
+			</div>
+			<div className="card text-center">
+				<img
+					src="https://live.staticflickr.com/4576/26876667779_4433cc9489_o.jpg"
+					className="card-img-top"
+					alt="..."
+				/>
+				<div className="card-body">
+					<h5 className="card-title">Card title</h5>
+					<p className="card-text">
+						Some quick example text to build on the card title and
+						make up the bulk of the cards content.
+					</p>
+					<a href="#" className="btn btn-primary">
+						Go somewhere
+					</a>
+				</div>
+			</div>
+		</div>
+	);
+};
---
+++ src/js/component/footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Footer = () => {
+	return (
+		<div className="text-center footerstyle">
+			<p> Copyright © Your Wedside 2018</p>
+		</div>
+	);
+};
